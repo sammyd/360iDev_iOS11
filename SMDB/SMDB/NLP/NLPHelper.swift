a4a2c2c53d7614c6c1a0326b259b46859c0cfe58
Sam Davies <sam@razeware.com>
2017-08-12 14:09:11 -0600

    Phase 20: Adding the CoreML sentiment analysis

--- SMDB/SMDB/NLP/NLPHelper.swift
+++ SMDB/SMDB/NLP/NLPHelper.swift
@@ -75,13 +75,38 @@ func getPeopleNames(text: String, block: (String) -> Void) {
 }
 
 func predictSentiment(text: String) -> Int? {
-  // To be replaced
-  return nil
+  let counts = tokenizeAndCountWords(text: text)
+  
+  let model = Sentiment()
+  let input = try! MLMultiArray(shape: [1, NSNumber(value: words.count)], dataType: .int32)
+  
+  for (index, counts) in counts.enumerated() {
+    input[index] = NSNumber(value: counts)
+  }
+  let prediction = try! model.prediction(wordCount: input)
+  let sentiment = prediction.sentiment
+  
+  return Int(sentiment)
 }
 
 func tokenizeAndCountWords(text: String) -> [Int] {
-   // To be replaced
-  return []
+  let tagger = NSLinguisticTagger(tagSchemes: [.lemma, .lexicalClass], options: 0)
+  tagger.string = text
+  let options: NSLinguisticTagger.Options = [.omitWhitespace,
+                                             .omitPunctuation,
+                                             .omitOther,
+                                             .joinNames]
+  let range = NSRange(text.startIndex..., in: text)
+  var wordCount = Array(repeating: 0, count: words.count)
+  tagger.enumerateTags(in: range, unit: .word, scheme: .lemma, options: options) { (tag, _, _) in
+    guard let tag = tag else { return }
+    let word = tag.rawValue
+    
+    if let index = words.index(of: word) {
+      wordCount[index] += 1
+    }
+  }
+  return wordCount
 }
 
 private let words = ["Austria", "Billy", "Chris", "Doctor", "Ellie", "I", "IDE", "Ms.", "Ning", "No", "Tiberius", "Xcode", "a", "all", "along", "also", "and", "annoy", "anything", "apocalypse", "aspiration", "at", "awful", "back", "background", "bad", "band", "be", "become", "believe", "better", "between", "big", "boo", "boring", "boy", "bug", "burger", "but", "by", "caffè", "can", "classic", "come", "confront", "contentious", "control", "crash", "cure", "dance", "dancing", "deadlock", "deadly", "die", "director", "disease", "do", "dragon", "dropping", "duet", "even", "eventually", "everything", "fabled", "famous", "fight", "film", "find", "finish", "fire", "follow", "for", "forget", "four", "from", "generation", "get", "go", "good", "great", "hah", "hat", "have", "he", "heartthrob", "hold", "horrible", "hour", "how", "iPhone", "ides", "in", "into", "involve", "it", "jaw", "joint", "joke", "journey", "keep", "killer", "leader", "learn", "legend", "life", "like", "literally", "little", "local", "lose", "loser", "loud", "love", "machine", "magnificent", "make", "many", "minus", "model", "money", "more", "most", "movie", "music", "need", "never", "nib", "night", "nil", "not", "now", "of", "oh", "on", "only", "oompah", "optional", "philosopher", "plague", "plot", "popular", "pretty", "punch", "really", "ride", "robot", "rule", "see", "service", "set", "show", "sing", "so", "spend", "star", "stop", "story", "straight", "stuff", "superb", "than", "that", "the", "there", "they", "thing", "think", "this", "three", "thrill", "through", "thumb", "time", "to", "total", "truly", "up", "use", "want", "watchdog", "way", "western", "what", "who", "why", "ya", "you", "young", "zombie"]
