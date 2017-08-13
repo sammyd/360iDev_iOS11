bc99fb0b10e5cdd56c7025029f49dc36ce5059c7
Sam Davies <sam@razeware.com>
2017-08-13 05:31:57 -0600

    Phase 0.3: Adding encoding to UIColor
    Won't do decoding cos it's hard

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -1,12 +1,27 @@
-import Cocoa
+import UIKit
 
-struct Person: Codable {
-  let name: String
-  let age: Int
-  let favouriteColour: String
+
+extension UIColor: Encodable {
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.unkeyedContainer()
+    var color : (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) = (0, 0, 0, 0)
+    getRed(&color.r, green: &color.g, blue: &color.b, alpha: &color.a)
+    try container.encode(color.r)
+    try container.encode(color.g)
+    try container.encode(color.b)
+    try container.encode(color.a)
+  }
 }
 
-let p1 = Person(name: "julie", age: 43, favouriteColour: "orange")
+
+
+struct Person: Encodable {
+  let name: String
+  let age: Int
+  let favouriteColour: UIColor
+}
+
+let p1 = Person(name: "nancy", age: 26, favouriteColour: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
 
 let encoder = JSONEncoder()
 let data = try! encoder.encode(p1)
@@ -14,5 +29,5 @@ let data = try! encoder.encode(p1)
 let jsonString = String(bytes: data, encoding: .utf8)
 
 let decoder = JSONDecoder()
-try! decoder.decode(Person.self, from: data)
+//try! decoder.decode(Person.self, from: data)
 
