7ebae77f0a1d27d7841a69a5d549d83ed69e554c
Sam Davies <sam@razeware.com>
2017-08-13 04:51:04 -0600

    Phase 0: Codable—first example

---
+++ Codable.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+let str = "A string"
+
+let encoder = JSONEncoder()
+let data = try! encoder.encode([str])
+
+let jsonString = String(bytes: data, encoding: .utf8)
+
+let decoder = JSONDecoder()
+try! decoder.decode([String].self, from: data)
+
